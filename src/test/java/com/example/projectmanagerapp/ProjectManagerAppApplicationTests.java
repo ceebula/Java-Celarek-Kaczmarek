79439e53b7e73bbe4fb33caa164cb47248f43f29
Tomasz <tomaszlegionkaczmarek@gmail.com>
2026-05-14 09:38:16 +0200

    Add user integration tests

--- src/test/java/com/example/projectmanagerapp/ProjectManagerAppApplicationTests.java
+++ src/test/java/com/example/projectmanagerapp/ProjectManagerAppApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.projectmanagerapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("integration-test")
 class ProjectManagerAppApplicationTests {
 
     @Test
